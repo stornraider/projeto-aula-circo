c208234322dee1406cbd263071c1a4caab08b7a4
stornraider <jacielferreira@gmail.com>
2013-08-26 18:57:29 -0300

    Create Locutor.java

---
+++ Locutor.java
@@ -0,0 +1,21 @@
+/*
+ * Programa da aula 01 - Circo 
+ * Personagem do mundo real
+ * Classe é uma representação computacional de um objeto do mundo real
+ */
+
+/**
+ *
+ * @author Jaciel Ferreira da Silva 
+ */
+public class Locutor {
+    /* Cadeia de caracteres String */
+    public String cor; 
+    /* Para caracteres inteiro int */
+    public int idade; 
+    public char sexo;
+    /* Ponto Frutuante: double ou float*/
+    public float altura;
+    
+}
+
