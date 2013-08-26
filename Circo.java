41d2f2135c9d6534001e9f00963ae6b357f2196f
stornraider <jacielferreira@gmail.com>
2013-08-26 18:56:44 -0300

    Create Circo.java

---
+++ Circo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Jaciel ferreira 
+ * Control + Space = Alto completar
+ */
+public class Circo {
+
+    /**
+     * @param args the command line arguments
+     */
+    /** main -> principal */
+    public static void main(String[] args) { 
+        // TODO code application logic here
+        Locutor objLocutor1 = new Locutor();
+        objLocutor1.cor = 'preto';
+        objLocutor1.cor = '34';
+        
+    }
+}
